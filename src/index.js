f547bfacf37ea5e986e1a231989310ca5e9c964f
Pavel16rus <kryuchkov.p@gmail.com>
2022-10-20 12:37:51 +0300

    hidden HW temp

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ console.log("Launching script");
 const anchors = document.querySelectorAll(".thumbnails-anchor");
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
+const mainClass = document.querySelector(".main-class");
+const HIDDEN = "hidden";
+const button = document.querySelector(".hide-button");
 function setDetails(anchor) {
     detailsImage.setAttribute('src', anchor.getAttribute('data-details-image'));
     detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
@@ -10,5 +13,16 @@ for(let i = 0; i<anchors.length; i++) {
     anchors[i].addEventListener("click", function() {
         console.log("event - click on", anchors[i]);
         setDetails(anchors[i]);
+        showDetails();
     })
+}
+button = button.addEventListener("click", function() {
+    hideDetails();
+})
+
+function showDetails () {
+    mainClass.classList.remove(HIDDEN);
+}
+function hideDetails() {
+    mainClass.classList.add(HIDDEN);
 }
