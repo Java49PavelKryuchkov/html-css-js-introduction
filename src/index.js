ab40b624fd3b64f618795ec8cf8356bc4536fbad
Pavel16rus <kryuchkov.p@gmail.com>
2022-10-25 21:22:48 +0300

    animation + audio element final edit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ const anchors = document.querySelectorAll(".thumbnails-anchor");
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
 const mainClass = document.querySelector(".main-class");
+const detailsContainer = document.querySelector(".details-container");
+const detailsAudio = document.querySelector(".details-audio");
 const HIDDEN = "hidden";
 const button = document.querySelector(".hide-button");
+const IS_POINT = "is-point";
 function setDetails(anchor) {
     detailsImage.setAttribute('src', anchor.getAttribute('data-details-image'));
+    detailsAudio.setAttribute('src', anchor.getAttribute('data-details-audio'));
     detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
 }
+
+
 for(let i = 0; i<anchors.length; i++) {
     anchors[i].addEventListener("click", function() {
         console.log("event - click on", anchors[i]);
@@ -22,7 +28,17 @@ button = button.addEventListener("click", function() {
 
 function showDetails () {
     mainClass.classList.remove(HIDDEN);
+    detailsContainer.classList.add(IS_POINT);
+    setTimeout(function () {
+        detailsContainer.classList.remove(IS_POINT);
+    }, 1)
+    detailsAudio.play();
+    setTimeout(function () {
+        detailsAudio.pause();
+    }, 10000)
+
 }
+
 function hideDetails() {
     mainClass.classList.add(HIDDEN);
 }
