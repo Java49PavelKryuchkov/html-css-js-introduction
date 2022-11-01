1f89936e60fcb3386728920e65a3ba5c7da5f8f4
Pavel16rus <kryuchkov.p@gmail.com>
2022-11-01 17:32:48 +0200

    js language introduction

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
-let number = 0;
 function sumDigits(number) {
+    if (number < 0) {
+        number = number *- 1;
+    }
     let resid = 0;
     let sum = 0;
-    let sum1 = 0;
-    let sum2 = 0;
    do {
     resid = number%10;
-   if (resid < 0) {
-        resid *=-1;
-    sum1 = sum1 + resid;
-    number = ~~(number/10);
-    } else {
-    sum2 = sum2 + resid;
+    sum = sum + resid;
     number = ~~(number/10);
    }
-   sum = sum1 + sum2;
-}
-   while (number != 0)
-    return sum;
-}
-console.log(sumDigits(12-34));
+
+   while (number != 0);
+    return sum; }
+console.log(sumDigits(-1234));
