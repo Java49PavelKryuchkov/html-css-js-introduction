9854ab3387ebce25b53aa754a5187a9aa28d59ae
Pavel16rus <kryuchkov.p@gmail.com>
2022-11-17 21:48:12 +0200

    wordle final edit

--- src/wordle.js
+++ src/wordle.js
@@ -1,39 +1,74 @@
-const word = "table";
+const words = ["pappy", "beach", "apple", "react", "basis",
+ "anger", "hello", "dress"];
+ let word;
 const N_LETTERS = 5;
 const letterElements = document.querySelectorAll(".letter-guess");
-let attemptsCounter = document.querySelector(".counter-number");
-let text = document.querySelector(".counter-text");
-let count = 6;
-attemptsCounter.innerHTML = count;
+const trialsElement = document.querySelector(".guess-trials");
+const gameOverElement = document.querySelector(".game-over-message");
+const playAgainElement = document.getElementById("play-again");
+let flGameOver = false;
+const INITIAL_TRIALS = 6;
+let trials = INITIAL_TRIALS;
+function showTrialsMessage(trials) {
+    
+        trialsElement.innerHTML = 
+        `remained ${trials} guess trials`;
+    
+   
+}
+function startGame() {
+    let index = Math.floor(Math.random() * words.length);
+    word = words[index];
+    trials = INITIAL_TRIALS
+    showTrialsMessage(trials);
+    gameOverElement.innerHTML ='';
+    playAgainElement.style.display='none';
+    letterElements.forEach(e => e.innerHTML='');
+    flGameOver = false;
+}
 function onChange(event) {
     const wordGuess = event.target.value;
+    
+    if (flGameOver) {
+        alert("game is over");
+        return;
+    }
+    trials--;
+    showTrialsMessage(trials);
+     
     if (wordGuess.length != N_LETTERS) {
-        alert(`A word should include ${N_LETTERS} letters`);
+        alert(`A word should contain ${N_LETTERS} letters`)
     } else {
-        let wordAr = Array.from(wordGuess);
+        const wordAr = Array.from(wordGuess);
         wordAr.forEach((l, i) => letterElements[i].innerHTML = l);
-        const colors = wordAr.map((l, i) => {
-            let index = word.indexOf(l);
+        const colors = wordAr.map((letter, i) => {
+            let index = word.indexOf(letter);
             let res = 'red';
-            if (index > -1) {
-                res = index == i ? 'green' : 'yellow';
+            if (index  > -1) {
+                res = letter == word[i] ? 'green' : 'yellow'
             }
             return res;
         })
-        colors.forEach((c, i) => letterElements[i].style.color=c);
-    if (wordGuess == word) {
-        text.innerHTML = "Congratulations - you have guessed word";
-        text.style.color = 'green';
-        attemptsCounter.innerHTML = "";
+        colors.forEach((c, i) =>
+         letterElements[i].style.color=c)
     }
-        else if (wordGuess != word && count > 1) {
-            count--;
-            attemptsCounter.innerHTML = count;
-    } else if (wordGuess != word && count <= 1){
-            text.innerHTML = "Sorry, your guess trials are ended up";
-            text.style.color = 'red';
-            attemptsCounter.innerHTML = "";
-    } 
+    const res = wordGuess == word;
+    if (trials == 0 || res) {
+        endGame(res);
+    }
+    
 }
-
+function endGame(isSuccess) {
+    if (isSuccess) {
+        gameOverElement.innerHTML =  "Congratulations you are winner";
+        gameOverElement.style.color = "green"
+    } else {
+        gameOverElement.innerHTML =  "Sorry you are loser";
+        gameOverElement.style.color = "red"
+    }
+   
+   playAgainElement.style.display='block';
+   trialsElement.innerHTML = '';
+   flGameOver = true;
 }
+startGame()
