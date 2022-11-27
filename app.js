32ec43c4750bd0b83c7a9cd9c43e77f2f98f8ef0
Pavel16rus <kryuchkov.p@gmail.com>
2022-11-27 21:06:45 +0200

    HW19

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
-function createEmployee (inDigits, minSalary, maxSalary) {
-    return {inDigits, minSalary, maxSalary};
+function createEmployee (inDigits, minSalary, maxSalary, minBirthYear, maxBirthYear) {
+    let power = getPower(inDigits);
+    let randomID = Math.floor(power + Math.random() * 9*power);
+    let randomSalary = Math.round(Math.random() * (maxSalary - minSalary + 1) ) + minSalary;
+    let randomYear = Math.round(Math.random() * (maxBirthYear - minBirthYear + 1) ) + minBirthYear;
+    return {randomID, randomSalary, randomYear};
 }
- function arrayOfObjects(employee, numberOfEmployees) {
+function getPower(nDigits) {
+    let res = 1;
+    let count = nDigits-1;
+    while (count != 0) {
+        res *=10;
+        count--;
+    }
+    return res;
+}
+
+ function noname(inDigits, minSalary, maxSalary, minBirthYear, maxBirthYear, nEmpl) {
     let Ar = [];
-    while (Ar.length != numberOfEmployees) {
-            Ar.push(employee);
+    while (Ar.length != nEmpl) {
+            Ar.push(createEmployee(inDigits, minSalary, maxSalary, minBirthYear, maxBirthYear));
         }
-        return Ar;
- }
+    return Ar;
+     }
+function getAverageAge(emplAr) {
+    let ageAr = emplAr.reduce((res, cur) => {
+        res.push(new Date().getFullYear() - cur.randomYear);
+        return res;
+    }, []);
+    let avgAge = ageAr.reduce((res, cur)=> {
+        let avg = Math.round((res + cur) / 2);
+        return avg;
+    }, 0)
+    return avgAge;
+}
 
- function noname(inDigits, minSalary, maxSalary, nEmpl) {
-    let randomID = Math.round(Math.random() * inDigits * 10000);
-    let somebody = createEmployee(randomID, minSalary, maxSalary);
-    let createAr = arrayOfObjects(somebody, nEmpl);
-    return createAr;
- }
+function getEmployeesBySalary (emplAr) {
+    let a = emplAr.sort((a, b) => {
+        return a.randomSalary - b.randomSalary;
+    })
+    return a;
+}
 
- console.log(noname(1000, 5000, 30000, 7));
+
+//  console.log(getAverageAge(noname(5, 5000, 30000, 1990, 2010, 10)));
+console.log(getEmployeesBySalary(noname(5, 5000, 30000, 1990, 2010, 10)));
