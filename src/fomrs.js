4626f8c4b506fdc7d5b89429dc9fe8f66233f04a
Pavel16rus <kryuchkov.p@gmail.com>
2022-12-03 18:36:49 +0200

    1

--- src/fomrs.js
+++ src/fomrs.js
@@ -1,4 +1,7 @@
 const inputElements = document.querySelectorAll(".form-class [name]");
+const salaryElement = document.getElementById("salary-message");
+const salaryDesign = document.getElementById("salary-design");
+
 function onSubmit(event) {
     event.preventDefault();
     const employee = Array.from(inputElements).reduce((res, cur) => {
@@ -10,7 +13,11 @@ function onSubmit(event) {
 function onChange(event) {
     if(event.target.name == "salary") {
         if(+event.target.value<1000 || +event.target.value>10000) {
-            event.target.value = ''
+            event.target.value = '';
+            salaryElement.hidden = false;
+            salaryDesign.bacgroundColor = 'red';
+        } else {
+            salaryElement.hidden = true;
         }
     }
 }
